a990de60125554160535fe1baae77130379b8bf6
chaitanya-kanase <chaitanyakanase1@gmail.com>
2026-06-10 18:46:59 +0530

    :Added the correct backend url

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://video-conferencing-app-1-vljk.onrender.com" :
+    "https://video-conferencing-app-rr85.onrender.com" :
 
     "http://localhost:8000"
 
